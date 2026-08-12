17ea405cc621b0122d3070354b1cbf643aabae31
technops <founder.technops@gmail.com>
2026-08-11 22:34:27 -1100

    base version

---
+++ soundmoney/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace soundmoney.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
